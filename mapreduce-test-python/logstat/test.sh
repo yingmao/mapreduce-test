1fcd2b261533971512edfd2c8eb9e17796b0688b
yingmao <giggle.my@gmail.com>
2026-02-03 18:35:29 -0500

    Refactor test.sh to use variables for Hadoop commands

--- mapreduce-test-python/logstat/test.sh
+++ mapreduce-test-python/logstat/test.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
+set -e
+
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/logstat/mapper.py -mapper ../../mapreduce-test-python/logstat/mapper.py \
--file ../../mapreduce-test-python/logstat/reducer.py -reducer ../../mapreduce-test-python/logstat/reducer.py \
--input /logstat/input/* -output /logstat/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/output/
+
+HDFS="/usr/local/hadoop/bin/hdfs"
+HADOOP="/usr/local/hadoop/bin/hadoop"
+STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+$HDFS dfs -rm -r -f /logstat/input/
+$HDFS dfs -rm -r -f /logstat/output/
+$HDFS dfs -mkdir -p /logstat/input/
+$HDFS dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat/input/
+
+# If mapper/reducer are executable with the python3 shebang, this is enough.
+# Otherwise, change -mapper/-reducer to "python3 ...".
+$HADOOP jar "$STREAMING_JAR" \
+  -file ../../mapreduce-test-python/logstat/mapper.py \
+  -mapper ../../mapreduce-test-python/logstat/mapper.py \
+  -file ../../mapreduce-test-python/logstat/reducer.py \
+  -reducer ../../mapreduce-test-python/logstat/reducer.py \
+  -input /logstat/input/* \
+  -output /logstat/output/
+
+$HDFS dfs -cat /logstat/output/part-00000
+
+$HDFS dfs -rm -r -f /logstat/input/
+$HDFS dfs -rm -r -f /logstat/output/
+
 ../../stop.sh
