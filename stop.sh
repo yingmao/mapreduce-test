a1866b1c696b1a4ae4627a01cab126e2bb84311e
yingmao <giggle.my@gmail.com>
2026-02-03 18:50:12 -0500

    Refactor stop.sh to improve shutdown order and error handling

--- stop.sh
+++ stop.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-/usr/local/hadoop/sbin/stop-dfs.sh
-/usr/local/hadoop/sbin/stop-yarn.sh
-/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver
+set -e
+
+HADOOP_HOME="/usr/local/hadoop"
+MAPRED="$HADOOP_HOME/bin/mapred"
+
+# Stop JobHistory first (reduces YARN activity)
+"$MAPRED" --daemon stop historyserver || true
+
+# If your Hadoop build ignores this env var, it does no harm.
+export YARN_STOP_TIMEOUT=30
+
+# Stop YARN next
+"$HADOOP_HOME/sbin/stop-yarn.sh" || true
+
+# Stop HDFS last
+"$HADOOP_HOME/sbin/stop-dfs.sh" || true
