d089399c3c859a591251f21fc3be82019d658679
yingmao <giggle.my@gmail.com>
2026-02-03 18:54:51 -0500

    Refactor test.sh to use variables and improve error handling

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
+set -e
+
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../mapreduce-test-python/logstat2/mapper.py \
--file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py \
--input /logstat2/input/* -output /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
+
+HDFS="/usr/local/hadoop/bin/hdfs"
+HADOOP="/usr/local/hadoop/bin/hadoop"
+STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+$HDFS dfs -rm -r -f /logstat2/input/
+/logstat2/output/ 2>/dev/null || true
+$HDFS dfs -rm -r -f /logstat2/output/
+$HDFS dfs -mkdir -p /logstat2/input/
+$HDFS dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
+
+$HADOOP jar "$STREAMING_JAR" \
+  -file ../../mapreduce-test-python/logstat2/mapper.py \
+  -mapper ../../mapreduce-test-python/logstat2/mapper.py \
+  -file ../../mapreduce-test-python/logstat2/reducer.py \
+  -reducer ../../mapreduce-test-python/logstat2/reducer.py \
+  -input /logstat2/input/* \
+  -output /logstat2/output/
+
+$HDFS dfs -cat /logstat2/output/part-00000
+
+$HDFS dfs -rm -r -f /logstat2/input/
+$HDFS dfs -rm -r -f /logstat2/output/
+
 ../../stop.sh
