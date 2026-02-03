aeb91de724003807f13c101b256528bc46ee30d6
yingmao <giggle.my@gmail.com>
2026-02-03 18:08:49 -0500

    Update Hadoop streaming jar version in test script

--- mapreduce-test-python/grep/test.sh
+++ mapreduce-test-python/grep/test.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
-base_dir=$(cd "$(dirname "$0")";pwd)
+set -e
+
+base_dir=$(cd "$(dirname "$0")"; pwd)
+
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/output/
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r -f /grep/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r -f /grep/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /grep/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/test.txt /grep/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/grep/mapper.py -mapper "../../mapreduce-test-python/grep/mapper.py world" \
--file ../../mapreduce-test-python/grep/reducer.py -reducer ../../mapreduce-test-python/grep/reducer.py \
--input /grep/input/* -output /grep/output/
+
+STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+# Option A: rely on shebang + executable bit
+# chmod +x ../../mapreduce-test-python/grep/mapper.py ../../mapreduce-test-python/grep/reducer.py
+
+/usr/local/hadoop/bin/hadoop jar "$STREAMING_JAR" \
+  -file ../../mapreduce-test-python/grep/mapper.py \
+  -mapper "../../mapreduce-test-python/grep/mapper.py world" \
+  -file ../../mapreduce-test-python/grep/reducer.py \
+  -reducer ../../mapreduce-test-python/grep/reducer.py \
+  -input /grep/input/* \
+  -output /grep/output/
+
 /usr/local/hadoop/bin/hdfs dfs -cat /grep/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /grep/output/
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r -f /grep/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r -f /grep/output/
+
 ../../stop.sh
