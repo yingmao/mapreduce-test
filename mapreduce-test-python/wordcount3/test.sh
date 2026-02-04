6404c4ae78ba9cff0bf4204675ca4ecafd363fe8
yingmao <giggle.my@gmail.com>
2026-02-03 19:19:57 -0500

    Refactor wordcount test script for clarity and updates
    Refactor test script to use variables for HDFS and Hadoop commands, and update to use new mapper and reducer file names.

--- mapreduce-test-python/wordcount3/test.sh
+++ mapreduce-test-python/wordcount3/test.sh
@@ -1,26 +1,42 @@
 #!/bin/sh
+set -e
+
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount3/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount3/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /wordcount3/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount3/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/wordcount3/mapper-1.py -mapper ../../mapreduce-test-python/wordcount3/mapper-1.py \
--file ../../mapreduce-test-python/wordcount3/reducer-1.py -reducer ../../mapreduce-test-python/wordcount3/reducer-1.py \
--input /wordcount3/input/* -output /wordcount3/output/
 
+HDFS="/usr/local/hadoop/bin/hdfs"
+HADOOP="/usr/local/hadoop/bin/hadoop"
+STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
 
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/wordcount3/mapper-2.py -mapper ../../mapreduce-test-python/wordcount3/mapper-2.py \
--file ../../mapreduce-test-python/wordcount3/reducer-2.py -reducer ../../mapreduce-test-python/wordcount3/reducer-2.py \
--input /wordcount3/output/* -output /wordcount3-2/output/
+# Clean
+$HDFS dfs -rm -r -f /wordcount3/input/  || true
+$HDFS dfs -rm -r -f /wordcount3/output/ || true
+$HDFS dfs -rm -r -f /wordcount3-2/output/ || true
 
+# Input
+$HDFS dfs -mkdir -p /wordcount3/input/
+$HDFS dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount3/input/
 
+# Stage 1
+$HADOOP jar "$STREAMING_JAR" \
+  -files ../../mapreduce-test-python/wordcount3/mapper1.py,../../mapreduce-test-python/wordcount3/reducer1.py \
+  -mapper ../../mapreduce-test-python/wordcount3/mapper1.py \
+  -reducer ../../mapreduce-test-python/wordcount3/reducer1.py \
+  -input /wordcount3/input/* \
+  -output /wordcount3/output/
 
+# Stage 2
+$HADOOP jar "$STREAMING_JAR" \
+  -files ../../mapreduce-test-python/wordcount3/mapper2.py,../../mapreduce-test-python/wordcount3/reducer2.py \
+  -mapper ../../mapreduce-test-python/wordcount3/mapper2.py \
+  -reducer ../../mapreduce-test-python/wordcount3/reducer2.py \
+  -input /wordcount3/output/* \
+  -output /wordcount3-2/output/
 
+$HDFS dfs -cat /wordcount3-2/output/part-00000 | tail -10
 
-/usr/local/hadoop/bin/hdfs dfs -cat /wordcount3-2/output/part-00000 | tail -10
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount3/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount3/output/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount3-2/output/
+# Clean up
+$HDFS dfs -rm -r -f /wordcount3/input/
+$HDFS dfs -rm -r -f /wordcount3/output/
+$HDFS dfs -rm -r -f /wordcount3-2/output/
+
 ../../stop.sh
