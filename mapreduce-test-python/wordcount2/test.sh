1a1d520c08fe91433cedaa818316c17cf3dfe710
yingmao <giggle.my@gmail.com>
2026-02-03 19:07:52 -0500

    Refactor test script to use variables for commands

--- mapreduce-test-python/wordcount2/test.sh
+++ mapreduce-test-python/wordcount2/test.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
+set -e
+
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount2/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /wordcount2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount2/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/wordcount2/mapper.py -mapper ../../mapreduce-test-python/wordcount2/mapper.py \
--file ../../mapreduce-test-python/wordcount2/reducer.py -reducer ../../mapreduce-test-python/wordcount2/reducer.py \
--input /wordcount2/input/* -output /wordcount2/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /wordcount2/output/part-00000 | tail -100
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount2/output/
+
+HDFS="/usr/local/hadoop/bin/hdfs"
+HADOOP="/usr/local/hadoop/bin/hadoop"
+STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+$HDFS dfs -rm -r -f /wordcount2/input/
+$HDFS dfs -rm -r -f /wordcount2/output/
+$HDFS dfs -mkdir -p /wordcount2/input/
+$HDFS dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount2/input/
+
+$HADOOP jar "$STREAMING_JAR" \
+  -files ../../mapreduce-test-python/wordcount2/mapper.py,../../mapreduce-test-python/wordcount2/reducer.py \
+  -mapper ../../mapreduce-test-python/wordcount2/mapper.py \
+  -reducer ../../mapreduce-test-python/wordcount2/reducer.py \
+  -input /wordcount2/input/* \
+  -output /wordcount2/output/
+
+$HDFS dfs -cat /wordcount2/output/part-00000 | tail -100
+
+$HDFS dfs -rm -r -f /wordcount2/input/
+$HDFS dfs -rm -r -f /wordcount2/output/
+
 ../../stop.sh
