7a0e71fc78e49952ab73cacfeb2a1611076cfd53
yingmao <giggle.my@gmail.com>
2026-02-03 19:49:40 -0500

    Update HDFS paths and file references in test script

--- mapreduce-test-python/wordcount2/test.sh
+++ mapreduce-test-python/wordcount2/test.sh
@@ -7,22 +7,22 @@ HDFS="/usr/local/hadoop/bin/hdfs"
 HADOOP="/usr/local/hadoop/bin/hadoop"
 STREAMING_JAR="/usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
 
-$HDFS dfs -rm -r -f /wordcount2/input/
-$HDFS dfs -rm -r -f /wordcount2/output/
-$HDFS dfs -mkdir -p /wordcount2/input/
-$HDFS dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount2/input/
+$HDFS dfs -rm -r -f /wordcount/input/
+$HDFS dfs -rm -r -f /wordcount/output/
+$HDFS dfs -mkdir -p /wordcount/input/
+$HDFS dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount/input/
 
 $HADOOP jar "$STREAMING_JAR" \
-  -files ../../mapreduce-test-python/wordcount/mapper.py \
+  -file ../../mapreduce-test-python/wordcount/mapper.py \
   -mapper ../../mapreduce-test-python/wordcount/mapper.py \
-  -files ../../mapreduce-test-python/wordcount/reducer.py \
+  -file ../../mapreduce-test-python/wordcount/reducer.py \
   -reducer ../../mapreduce-test-python/wordcount/reducer.py \
   -input /wordcount/input/* \
   -output /wordcount/output/
 
-$HDFS dfs -cat /wordcount2/output/part-00000 | tail -100
+$HDFS dfs -cat /wordcount/output/part-00000
 
-$HDFS dfs -rm -r -f /wordcount2/input/
-$HDFS dfs -rm -r -f /wordcount2/output/
+$HDFS dfs -rm -r -f /wordcount/input/
+$HDFS dfs -rm -r -f /wordcount/output/
 
 ../../stop.sh
